a367ce47327fa23f524c7e7df0740dcda7d19bd8
Tatiana <ef.tatiana@mail.ru>
2024-03-27 00:18:21 +0300

    Changed to Parameterized Test

--- src/test/java/ru/netology/services/CalcServiceTest.java
+++ src/test/java/ru/netology/services/CalcServiceTest.java
@@ -1,26 +1,38 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CalcServiceTest {
-    @Test
-    public void countOfHolidaysMonthsTest1() {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/holidays.csv")
+    public void countOfHolidaysMonthsTest1(int income, int expenses, int threshold, int expected) {
         CalcService service = new CalcService();
-        int income = 100000;
-        int expenses = 60000;
-        int threshold = 150000;
+        //int income = 100000;
+        //int expenses = 60000;
+        //int threshold = 150000;
         int result = service.calculate(income, expenses, threshold);
-        Assertions.assertEquals(2, result);
+        Assertions.assertEquals(expected, result);
     }
 
-    @Test
-    public void countOfHolidaysMonthsTest2() {
-        CalcService service = new CalcService();
-        int income = 10000;
-        int expenses = 3000;
-        int threshold = 20000;
-        int result = service.calculate(income, expenses, threshold);
-        Assertions.assertEquals(3, result);
-    }
+    //     public void countOfHolidaysMonthsTest1() {
+//        CalcService service = new CalcService();
+//        int income = 100000;
+//        int expenses = 60000;
+//        int threshold = 150000;
+//        int result = service.calculate(income, expenses, threshold);
+//        Assertions.assertEquals(2, result);
+
+
+//   @Test
+//    public void countOfHolidaysMonthsTest2() {
+//        CalcService service = new CalcService();
+//        int income = 10000;
+//        int expenses = 3000;
+//        int threshold = 20000;
+//        int result = service.calculate(income, expenses, threshold);
+ //       Assertions.assertEquals(3, result);
+//    }
 }
